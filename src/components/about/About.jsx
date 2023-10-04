d8284825d0994da348f61ece24ef8f40631998f6
Bill Notturno <swissbones291@aim.com>
2023-10-04 14:02:00 -0400

    updated 10-4

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -35,7 +35,7 @@ const About = () => {
             <article className="about__card">
               <VscFolderLibrary className="about__icon" />
               <h5>Projects</h5>
-              <small>Business, Games, Quizzes, etc.</small>
+              <small>Business, Games, Quizzes, Entertainment, etc.</small>
             </article>
           </div>
 
