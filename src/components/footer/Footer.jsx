46efb1714b226c9e97a6a0e0a63bffdc6d72841a
Bill Notturno <swissbones291@aim.com>
2024-03-22 13:56:10 -0400

    footer update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
           <a href="#portfolio">Portfolio</a>
         </li>
         <li>
-          <a href="#testimonials">Testimonials</a>
+          <a href="#testimonials">Certifications</a>
         </li>
         <li>
           <a href="#contact">Contact</a>
